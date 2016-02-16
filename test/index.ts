739fc0e480c5e383426bcf5d49cf3947c7d2f922
Ben Southgate <bsouthga@gmail.com>
2016-02-16 15:50:29 -0500

    testing

---
+++ test/index.ts
@@ -0,0 +1,9 @@
+/// <reference path="../typings/main.d.ts" />
+import { expect } from 'chai';
+
+describe("gracl.ts", () => {
+  it("should expect true to be true", () => {
+    const a = true;
+    expect(a).to.equal(true);
+  });
+});
