552026ec2b0f38955711b6b0beed4ea6c4638765
Ben Southgate <bsouthga@gmail.com>
2016-02-16 14:39:49 -0500

    tsconfig + minification

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,9 +4,15 @@ echo "Installing type definitions..."
 typings install
 
 echo "compiling typescript..."
-./node_modules/typescript/bin/tsc -p ./src
+./node_modules/typescript/bin/tsc -p ./
 
 echo "Running browserify..."
 ./node_modules/browserify/bin/cmd.js ./dist/index.js \
   -o ./dist/gracl.js \
   -t [ babelify --presets [ es2015 ] ]
+
+echo "Minifying..."
+./node_modules/uglifyjs/bin/uglifyjs ./dist/gracl.js \
+  --screw-ie8 \
+  --mangle \
+  -o  ./dist/gracl.min.js
