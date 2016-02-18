160a6e5e4a7dd6433893ce3fedc9466043f714db
Ben Southgate <bsouthga@gmail.com>
2016-02-18 14:29:26 -0500

    dummy tests

---
+++ test/common.ts
@@ -0,0 +1,16 @@
+/// <reference path="../typings/main.d.ts" />
+import { expect } from 'chai';
+import common from '../lib/common';
+
+
+describe("Common functions", () => {
+
+  it("yes() should return true", () => {
+    expect(common.yes()).to.equal(true);
+  });
+
+  it("noop() should return void", () => {
+    expect(common.noop()).to.equal(void 0);
+  });
+
+});
