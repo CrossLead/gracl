59e8817a887e052b543dbd3d2dff768544f3915c
Ben Southgate <bsouthga@gmail.com>
2016-02-21 14:55:04 -0500

    fix export issues

--- lib/index.ts
+++ lib/index.ts
@@ -4,11 +4,10 @@ import { Subject } from './classes/Subject';
 import { Resource } from './classes/Resource';
 import { MemoryRepository } from './builtins/MemoryRepository';
 
-export * from './util';
-export * from './classes/Node';
-export * from './classes/Subject';
-export * from './classes/Resource';
-export * from './builtins/MemoryRepository';
+export { Node } from './classes/Node';
+export { Subject } from './classes/Subject';
+export { Resource } from './classes/Resource';
+export { MemoryRepository } from './builtins/MemoryRepository';
 
 export default {
   util,
