739a5f3c2d07555884a472665f3f2b974c504c41
Ben Southgate <bsouthga@gmail.com>
2016-02-21 13:48:57 -0500

    browserify doesn't like * exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,13 @@
-export * from './util';
-export * from './classes/Node';
-export * from './classes/Subject';
-export * from './classes/Resource';
-export * from './builtins/MemoryRepository';
+import * as util from './util';
+import { Node } from './classes/Node';
+import { Subject } from './classes/Subject';
+import { Resource } from './classes/Resource';
+import { MemoryRepository } from './builtins/MemoryRepository';
+
+export default {
+  util,
+  Node,
+  Subject,
+  Resource,
+  MemoryRepository
+}
