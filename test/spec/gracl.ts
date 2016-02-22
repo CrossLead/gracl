93f217f2cbad810a7df096ea205e1204b97f66c1
Ben Southgate <bsouthga@gmail.com>
2016-02-21 22:07:04 -0500

    namespace big imports

--- test/spec/gracl.ts
+++ test/spec/gracl.ts
@@ -1,21 +1,7 @@
 /// <reference path='../../typings/main.d.ts' />
 import { expect } from 'chai';
-
-import {
-  orgModel,
-  blogModel,
-  userModel,
-  teamModel,
-  postModel,
-  OrganizationSubject,
-  OrganizationResource,
-  Team,
-  User,
-  Blog,
-  Post
-} from '../classes/index';
-
-import { user, team, org, post, blog, uidReset } from '../helpers/index';
+import * as classes from '../classes/index';
+import * as helpers from '../helpers/index';
 
 describe('gracl', () => {
   let orgA,
@@ -34,47 +20,47 @@ describe('gracl', () => {
       postA1a;
 
   const resetTestData = async () => {
-    uidReset();
+    helpers.uidReset();
 
     // setup, add documents to models
-    orgA = org();
-    orgB = org();
+    orgA = helpers.org();
+    orgB = helpers.org();
 
-    teamA1 = team(orgB);
-    teamA2 = team(orgB);
-    teamA3 = team(orgB);
-    teamB1 = team(orgB);
+    teamA1 = helpers.team(orgB);
+    teamA2 = helpers.team(orgB);
+    teamA3 = helpers.team(orgB);
+    teamB1 = helpers.team(orgB);
 
-    userA1 = user([ teamA1, teamA2 ]);
-    userA2 = user([ teamA1, teamA3 ]);
-    userB1 = user([ teamB1 ]);
+    userA1 = helpers.user([ teamA1, teamA2 ]);
+    userA2 = helpers.user([ teamA1, teamA3 ]);
+    userB1 = helpers.user([ teamB1 ]);
 
-    blogA1 = blog(orgA);
-    blogB1 = blog(orgB);
+    blogA1 = helpers.blog(orgA);
+    blogB1 = helpers.blog(orgB);
 
-    postB1a = post(blogB1);
-    postB1b = post(blogB1);
-    postA1a = post(blogA1);
+    postB1a = helpers.post(blogB1);
+    postB1b = helpers.post(blogB1);
+    postA1a = helpers.post(blogA1);
 
     await Promise.all([
-      orgModel.saveEntity(orgA.id, orgA),
-      orgModel.saveEntity(orgB.id, orgB),
+      classes.orgModel.saveEntity(orgA.id, orgA),
+      classes.orgModel.saveEntity(orgB.id, orgB),
 
-      teamModel.saveEntity(teamA1.id, teamA1),
-      teamModel.saveEntity(teamA2.id, teamA2),
-      teamModel.saveEntity(teamA3.id, teamA3),
-      teamModel.saveEntity(teamB1.id, teamB1),
+      classes.teamModel.saveEntity(teamA1.id, teamA1),
+      classes.teamModel.saveEntity(teamA2.id, teamA2),
+      classes.teamModel.saveEntity(teamA3.id, teamA3),
+      classes.teamModel.saveEntity(teamB1.id, teamB1),
 
-      userModel.saveEntity(userA1.id, userA1),
-      userModel.saveEntity(userA2.id, userA2),
-      userModel.saveEntity(userB1.id, userB1),
+      classes.userModel.saveEntity(userA1.id, userA1),
+      classes.userModel.saveEntity(userA2.id, userA2),
+      classes.userModel.saveEntity(userB1.id, userB1),
 
-      blogModel.saveEntity(blogA1.id, blogA1),
-      blogModel.saveEntity(blogB1.id, blogB1),
+      classes.blogModel.saveEntity(blogA1.id, blogA1),
+      classes.blogModel.saveEntity(blogB1.id, blogB1),
 
-      postModel.saveEntity(postA1a.id, postA1a),
-      postModel.saveEntity(postB1a.id, postB1a),
-      postModel.saveEntity(postB1b.id, postB1b)
+      classes.postModel.saveEntity(postA1a.id, postA1a),
+      classes.postModel.saveEntity(postB1a.id, postB1a),
+      classes.postModel.saveEntity(postB1b.id, postB1b)
     ]);
 
   };
@@ -85,30 +71,30 @@ describe('gracl', () => {
 
   it('Retrieving document from repository should work', async() => {
     expect(
-      await orgModel.getEntity(orgA.id),
+      await classes.orgModel.getEntity(orgA.id),
       'Memory Repository should sucessfully set items'
     ).to.equal(orgA);
   });
 
 
   it('Resource.getParents() should return Resource instances of parent objects', async() => {
-    const resource = new Post(postA1a);
+    const resource = new classes.Post(postA1a);
     const [ parent ] = await resource.getParents();
-    expect(parent, 'Returned parent should be gracl node instance.').to.be.instanceof(Blog);
+    expect(parent, 'Returned parent should be gracl node instance.').to.be.instanceof(classes.Blog);
     expect(parent.getId(), 'Correct parent should be returned.').to.equal(blogA1.id);
   });
 
 
   it('Resource.allow(Subject, <perm>) -> subject can access resource.', async() => {
-    const resource = new Post(postA1a),
-          subject = new User(userA1);
+    const resource = new classes.Post(postA1a),
+          subject = new classes.User(userA1);
 
     const initialAllowed = await resource.isAllowed(subject, 'view');
 
     expect(
       await resource.allow(subject, 'view'),
       'Setting permission should return same resource type.'
-    ).to.be.instanceof(Post);
+    ).to.be.instanceof(classes.Post);
 
     const afterSetAllowed = await resource.isAllowed(subject, 'view');
 
@@ -118,16 +104,16 @@ describe('gracl', () => {
 
 
   it('Resource.allow(parentSubject, <perm>) -> child subject can access resource.', async() => {
-    const parentResource = new Blog(blogA1),
-          childResource = new Post(postA1a),
-          subject = new User(userA1);
+    const parentResource = new classes.Blog(blogA1),
+          childResource = new classes.Post(postA1a),
+          subject = new classes.User(userA1);
 
     const initialAllowed = await childResource.isAllowed(subject, 'view');
 
     expect(
       await parentResource.allow(subject, 'view'),
       'Setting permission for parentSubject should return same resource type.'
-    ).to.be.instanceof(Blog);
+    ).to.be.instanceof(classes.Blog);
 
     const afterSetAllowed = await childResource.isAllowed(subject, 'view');
 
@@ -137,17 +123,17 @@ describe('gracl', () => {
 
 
   it('parentResource.allow(parentSubject, <perm>) -> child subject can access child resource.', async() => {
-    const parentResource = new Blog(blogA1),
-          childResource  = new Post(postA1a),
-          parentSubject  = new Team(teamA1),
-          childSubject   = new User(userA1);
+    const parentResource = new classes.Blog(blogA1),
+          childResource  = new classes.Post(postA1a),
+          parentSubject  = new classes.Team(teamA1),
+          childSubject   = new classes.User(userA1);
 
     const initialAllowed = await childResource.isAllowed(childSubject, 'view');
 
     expect(
       await parentResource.allow(parentSubject, 'view'),
       'Setting permission for parentSubject should return same resource type.'
-    ).to.be.instanceof(Blog);
+    ).to.be.instanceof(classes.Blog);
 
     const afterSetAllowed = await childResource.isAllowed(childSubject, 'view');
 
@@ -157,9 +143,9 @@ describe('gracl', () => {
 
 
   it('Permissions should be visible through resource.getPermissionsHierarchy()', async() => {
-    const parentResource = new Blog(blogA1),
-          childResource = new Post(postA1a),
-          subject = new User(userA1);
+    const parentResource = new classes.Blog(blogA1),
+          childResource = new classes.Post(postA1a),
+          subject = new classes.User(userA1);
 
     await parentResource.allow(subject, 'view');
 
